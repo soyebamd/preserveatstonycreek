cb7d05406cc8f19c7d0e2a5650873514f1845c7e
soyeb <soyebahmed@gmail.com>
2024-04-12 12:34:24 +0530

    Modification

--- src/js/main.js
+++ src/js/main.js
@@ -5,10 +5,15 @@ $(".slide-show").slick({
   fade: true,
   autoplay: false,
   autoplaySpeed: 3000,
-  speed: 3000,
+  speed: 1000,
   infinite: true,
 
   dots: true,
+
+  arrows: true,
+
+  prevArrow: "#move-left",
+  nextArrow: "#move-right",
 
   focusOnSelect: true,
   pauseOnHover: false,
